9397cb30b38b886575c353521eceaa430b4ba414
Daniel B Sherry <dsherry@proxlearn.com>
2024-02-25 10:48:54 -0600

    useFetch added

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,19 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState("mario");
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs") // returns a promise
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
-  }, []); // dependency array
+  const {
+    error,
+    isPending,
+    data: blogs,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-      <button onClick={() => setName("luigi")}>change name</button>
-      <p>{name}</p>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
